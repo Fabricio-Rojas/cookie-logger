38979f772f1af847b6fced784f440ec09811dc4d
Fabricio Rojas <fabriciovmamanir@gmail.com>
2023-03-30 13:21:20 -0500

    added dialog 2

--- assets/script/index.js
+++ assets/script/index.js
@@ -5,9 +5,25 @@
 const dialogOne = document.querySelector('.dlg1');
 const dialogTwo = document.querySelector('.dlg2');
 
+const acceptBtn = document.querySelector('#acceptBtn');
+const settingsBtn = document.querySelector('#settingsBtn');
+const preferencesBtn = document.querySelector('#preferencesBtn');
+
 // Main Function
 
 setTimeout(() => {
     dialogOne.open = true;
-    console.log('open')
 }, 1000);
+
+acceptBtn.addEventListener('click', function() {
+    dialogOne.close();
+})
+
+settingsBtn.addEventListener('click', function() {
+    dialogTwo.open = true;
+})
+
+preferencesBtn.addEventListener('click', function() {
+    dialogOne.close();
+    dialogTwo.close();
+})
